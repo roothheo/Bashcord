03591eec073a22a9855169b95deba59852389115
null <116464968+roothheo@users.noreply.github.com>
2025-07-03 16:58:04 +0200

    FIX some errors

--- src/main/updater/http.ts
+++ src/main/updater/http.ts
@@ -50,7 +50,7 @@ async function calculateGitChanges() {
     return data.commits.map((c: any) => ({
         // github api only sends the long sha
         hash: c.sha.slice(0, 7),
-        author: c.author.login,
+        author: c.author?.login || "Unknown",
         message: c.commit.message.split("\n")[0]
     }));
 }
